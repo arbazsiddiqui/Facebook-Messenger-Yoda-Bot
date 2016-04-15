f6eba6871c1e4e507f937f1c4eb133d5615df651
arbaz <arbaz00@gmail.com>
2016-04-16 02:42:22 +0530

    done something

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post('/webhook/', function (req, res) {
                     console.log(result.status, result.body);
                     yodaSpeaks = result.body
                 });
-            sendTextMessage(sender, yodaSpeaks)
+            sendTextMessage(sender, yodaSpeaks.toString())
         }
     }
     res.sendStatus(200)
